8d0ea935a4c7f1fa445333199b5d0b198563bea1
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-20 07:29:53 +0530

    commit

---
+++ SumOfTwoNumbers.py
@@ -0,0 +1,5 @@
+# Input two numbers and find their sum
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+sum = n1 + n2
+print(f"The sum of {n1} and {n2} is {sum}.")
