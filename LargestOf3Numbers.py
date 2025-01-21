efa2fd8c97ace7c2dfc08aa5cb330f9b3dc054a8
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-21 08:42:44 +0530

    commit

---
+++ LargestOf3Numbers.py
@@ -0,0 +1,9 @@
+# Input three numbers
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+
+# Find the largest number
+largest = max(a, b, c)
+
+print("The largest number is:", largest)
