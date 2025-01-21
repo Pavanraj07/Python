6a7aab6d273dc32fbdb6439a7c4d0c3e77f1c0ec
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-21 08:39:20 +0530

    commit

---
+++ MultiplicationTable.py
@@ -0,0 +1,7 @@
+# Input the number
+num = int(input("Enter a number: "))
+
+# Generate multiplication table
+print(f"Multiplication Table for {num}:")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
