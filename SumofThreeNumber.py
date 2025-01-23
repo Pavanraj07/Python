416dee8f7181e00e589f54f6e02e4a373279b903
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-23 19:18:49 +0530

    commit

---
+++ SumofThreeNumber.py
@@ -0,0 +1,6 @@
+# Input two numbers and find their sum
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+n3 = float(input("Enter the third number: "))
+sum = n1 + n2 + n3
+print(f"The sum of {n1} and {n2} and {n3} is {sum}.")
