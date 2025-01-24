1044341f391bc10c1ba88cd8b0608fc0511461e5
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-24 07:53:35 +0530

    commit

---
+++ CheckSquareMatrix.py
@@ -0,0 +1,8 @@
+# Input matrix dimensions
+rows = int(input("Enter number of rows: "))
+cols = int(input("Enter number of columns: "))
+
+if rows == cols:
+    print("The matrix is a square matrix.")
+else:
+    print("The matrix is not a square matrix.")
