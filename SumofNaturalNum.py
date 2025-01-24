2cbbfd220548b37674831051f85746727f4c8dd0
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-24 08:04:02 +0530

    commit

---
+++ SumofNaturalNum.py
@@ -0,0 +1,4 @@
+# Calculate the sum of the first n natural numbers
+n = int(input("Enter a positive integer: "))
+sum_of_numbers = n * (n + 1) // 2
+print(f"The sum of the first {n} natural numbers is {sum_of_numbers}.")
