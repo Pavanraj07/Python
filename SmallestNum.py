b1f7c6acb282bca1bbb57e624d9bf6cceb62027c
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-29 19:19:59 +0530

    commit

---
+++ SmallestNum.py
@@ -0,0 +1,7 @@
+# Find the smallest number among three
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+smallest = min(a, b, c)
+print(f"The smallest number is {smallest}.")
