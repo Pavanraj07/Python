a7263b73388616a787b8f684e4993cf23d5e49e4
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-30 08:42:39 +0530

    commit

---
+++ RectanglePerimeter.py
@@ -0,0 +1,6 @@
+# Calculate the perimeter of a rectangle
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+perimeter = 2 * (length + width)
+print(f"The perimeter of the rectangle is {perimeter}.")
